de8695a5dce64a482c2130e5dee9c67642df2475
daanworks <daniel.babinszky@gmail.com>
2025-03-30 12:32:06 +0200

    added api

---
+++ src/app/api/message/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from "next/server";
+
+export const GET = async () => {
+  try {
+    const createdAt = new Date(Date.now()).toISOString().split("T")[0]
+    const jokeResponse = await fetch('https://api.sampleapis.com/jokes/goodJokes');
+    const joke = await jokeResponse.json();
+    return NextResponse.json(
+      {
+        createdAt,
+        message: 'HOLD',
+        joke: joke[0]
+      },
+      {
+        status: 200,
+      }
+    )
+  } catch (error: Error) {
+    return NextResponse.json({ error: (error as Error).message }, { status: 500 });
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,8 @@ import { ArcElement, Tooltip, Legend, Title, Chart as ChartJS } from 'chart.js';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXTwitter } from "@fortawesome/free-brands-svg-icons";
 import { config } from '@fortawesome/fontawesome-svg-core'
+import { faCode } from "@fortawesome/free-solid-svg-icons";
+
 config.autoAddCss = false
 
 ChartJS.register(ArcElement, Tooltip, Legend, Title);
@@ -15,31 +17,48 @@ export default function Home() {
   return (
     <div className="px-6 sm:px-0 w-full max-w-md md:max-w-lg lg:max-w-xl xl:max-w-2xl mx-auto flex flex-col min-h-screen justify-between">
       <div>
-        <div className='flex gap-3 items-center pt-12 pb-24'>
+        <div className='flex gap-3 items-center pt-8 pb-16'>
           <div className='lg:w-[68px] lg:h-[68px] md:w-[58px] md:h-[58px] w-[50px] h-[50px] relative'>
             <Image src='/logo.png' alt='logo' fill />
           </div>
         </div>
         <div className='text-3xl pb-12'>
-          Empowering smarter financial decisions for a more secure future
+          Providing a data-driven API on Bitcoin market trends for individuals and businesses
         </div>
         <div className='flex flex-col sm:flex-row gap-2'>
-          <div className='font-sans text-sm py-3 px-6 border max-w-max bg-blue text-white rounded-full cursor-pointer'>Browse Publications</div>
-          <div className='py-3 px-6 border max-w-max rounded-full cursor-pointer flex items-center'>
-            <div className='font-sans text-sm mr-1'>Follow Us on</div>
-            <div className='w-[16px] h-[16px]'>
-              <FontAwesomeIcon icon={faXTwitter} size='xs' />
+          <Link href='https://x.com/cultusventures' target="_blank" rel="noopener noreferrer">
+            <div className='py-3 px-6 border max-w-max rounded-full cursor-pointer flex items-center bg-blue text-white'>
+              <div className='font-sans text-sm mr-1'>Get Access</div>
+              <div className='w-[16px] h-[12px]'>
+                <FontAwesomeIcon icon={faCode} size='xs' />
+              </div>
             </div>
-          </div>
+          </Link>
+          <Link href='mailto:cultusventures@gmail.com' className='no-underline' target="_blank" rel="noopener noreferrer">
+            <div className='font-sans text-black text-sm py-3 px-6 border max-w-max rounded-full cursor-pointer'>
+              Contact us
+            </div>
+          </Link>
         </div>
-        <div className='font-sans text-xl underline pb-2 pt-24'>What We Do</div>
-        <div>We create engaging articles to educate and inspire our readers. <Link href='https://medium.com/' rel="noopener noreferrer" target="_blank">Our publications</Link> focus on money psychology, helping people gain deeper self-awareness and take control of their financial decisions.</div>
+        <div className='font-sans text-xl underline pb-2 pt-16'>What We Do</div>
+        <div>
+          Our mission is to educate and empower people to navigate the Bitcoin ecosystem with confidence. We keep it simple — clear, practical knowledge on the world’s hardest money.
+        </div>
         <div className='font-sans text-xl underline pb-2 pt-12'>Public Portfolio</div>
         <div className='pb-12'>Our portfolio is exclusively focused on Bitcoin, reflecting our belief in its long-term potential and its role in achieving financial freedom. By sharing this, we aim to provide insights into our investment philosophy.</div>
+        <div className='pb-12 gap-2 flex items-center font-sans'>
+          <div className='max-w-max'>
+            <div className='relative'>
+              <div className='absolute translate-x-[-50%] translate-y-[-50%] top-[50%] left-[50%] text-green'>•</div>
+              <div className='animate-pulse text-green'>•</div>
+            </div>
+          </div>
+          <div>Since our last buy, our portfolio is up by 309%</div>
+        </div>
       </div>
       <div className='font-sans text-xs pb-6'>
         <div className='pb-2'>All information is shared for educational purposes only and does not constitute financial advice.</div>
-        <div>© 2024, Cultus Ventures</div>
+        <div>© {new Date().getFullYear()}, Cultus Ventures</div>
       </div>
     </div>
   );

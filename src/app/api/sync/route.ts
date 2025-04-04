250ca1687c09db39867d1baad8ce8c5d3a2e7d00
daanworks <daniel.babinszky@gmail.com>
2025-04-04 11:06:17 +0200

    added gemini

--- src/app/api/sync/route.ts
+++ src/app/api/sync/route.ts
@@ -1,15 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "../../../../lib/prisma";
+import { GoogleGenAI } from "@google/genai";
 
 export const GET = async (req: NextRequest) => {
   const auth = req.headers.get('authorization')
   if (auth !== `Bearer ${process.env.CRON_SECRET}`) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 } as any);
   }
+
+  const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+
   try {
+    const response = await ai.models.generateContent({
+      model: "gemini-2.0-flash",
+      contents: "Explain how AI works in a few words",
+    })
+    const content = response.text as string
     await prisma.analysis.create({
       data: {
-        content: 'HOLD'
+        content
       }
     })
     return NextResponse.json({ success: true }, { status: 200 } as any)

fbc57cdabd287d2e16b49e1a1714088c3275cde1
daanworks <daniel.babinszky@gmail.com>
2025-04-15 14:21:22 +0200

    added packages, rules and husky config

--- src/app/api/analysis/route.ts
+++ src/app/api/analysis/route.ts
@@ -1,12 +1,14 @@
-import { NextResponse } from "next/server";
-import { AnalysisService } from "@/services";
+import { NextResponse } from 'next/server'
+import { AnalysisService } from '@/services'
 
 export const GET = async () => {
   try {
-    const response = await AnalysisService.getLatest();
-    return NextResponse.json(response, { status: 200 } as any
-    )
+    const response = await AnalysisService.getLatest()
+    return NextResponse.json(response, { status: 200 })
   } catch (error) {
-    return NextResponse.json({ error: (error as Error).message }, { status: 500 } as any);
+    return NextResponse.json(
+      { error: (error as Error).message },
+      { status: 500 },
+    )
   }
 }

4b795cebe26392edf2ab8a66ba2e3291eedb8e26
daanworks <daniel.babinszky@gmail.com>
2025-04-16 09:41:31 +0200

    refactored services

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,8 +1,10 @@
 import { PrismaClient, Analysis, ApiKey, ShortDescription } from '@prisma/client'
 import { GenerateContentResponse, GoogleGenAI } from '@google/genai'
+import { MailerooClient } from 'maileroo'
 
 const prisma = new PrismaClient()
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY })
+const maileroo: MailerooClient = MailerooClient.getClient(process.env.MAILEROO_API_KEY)
 
 export const AnalysisService = {
   getLatest: async (): Promise<Analysis | null> => {
@@ -60,5 +62,17 @@ export const AiService = {
       contents: prompt,
     })
     return generatedContent.text || ''
+  },
+}
+
+export const MailService = {
+  sendMail: async (to: string): Promise<void> => {
+    const html: string = `<div style="font-family:Arial,sans-serif;max-width:600px;margin:auto;padding:20px;background:#f9f9f9;border:1px solid #ddd;border-radius:8px;"><h2 style="color:#333;">🎉 Thank you for subscribing!</h2><p style="font-size:16px;color:#555;">Here is your personal API key:</p><pre style="background:#eee;padding:10px;border-radius:5px;font-size:18px;font-weight:bold;color:#000;">{{API_KEY}}</pre><p style="font-size:14px;color:#777;">Please keep this key safe. You’ll need it to access our API services.</p><hr style="margin:20px 0;"><p style="font-size:12px;color:#aaa;">If you didn’t expect this email, feel free to ignore it or contact support.</p></div>`
+    await maileroo
+      .setFrom('Cultus Ventures', 'no-reply@cultusventures.com')
+      .setTo('Cultus Ventures API User', to)
+      .setSubject("Thanks for purchasing, here's your API key")
+      .setHtml(html)
+      .sendBasicEmail()
   },
 }

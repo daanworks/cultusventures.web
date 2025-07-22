207d8e1f815bc267d85f4ee2e265fa831b00c692
daanworks <daniel.babinszky@gmail.com>
2025-07-22 09:45:35 +0200

    CVWEB-35: deleted unused services

--- src/app/api/sentiment/route.ts
+++ src/app/api/sentiment/route.ts
@@ -1,11 +1,8 @@
 import { NextResponse } from 'next/server'
-import { SentimentService } from '@/services'
-import { Sentiment } from '@prisma/client'
 
 export const GET = async () => {
   try {
-    const response: Sentiment | null = await SentimentService.getLatest()
-    return NextResponse.json(response, { status: 200 })
+    return NextResponse.json({ success: true }, { status: 200 })
   } catch (error) {
     return NextResponse.json({ error: (error as Error).message }, { status: 500 })
   }
